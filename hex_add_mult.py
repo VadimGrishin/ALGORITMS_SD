0e074f247892b1d6a73b2f22b7e22dac3e4f219c
Vadim <kran1@inbox.ru>
2019-01-26 11:05:52 +0300

    действия с hex

--- hex_add_mult.py
+++ hex_add_mult.py
@@ -4,22 +4,19 @@ digit_val = dict((digit[i], i) for i in range(len(digit)))
 
 def add_nn(x, y, add_mult):
     # add_mult = 1 сложение чисел x и y, = 0 - умножение цифры x на число y
-    res = []
-    shift = '0'
+    res, shift = [], '0'
     while x and add_mult or y:
         if add_mult:
             s = digit_val[x.pop() if x else '0'] + digit_val[y.pop() if y else '0'] + digit_val[shift]
         else:
             s = digit_val[x] * digit_val[y.pop() if y else '0'] + digit_val[shift]
-        res = [digit[s % len(digit)]] + res
-        shift = digit[s // len(digit)]
+        res, shift = [digit[s % len(digit)]] + res, digit[s // len(digit)]
 
     return ([shift] if digit_val[shift] else []) + res
 
 
 def mult_nn(p1, p2):
-    shift = []
-    s = []
+    shift, s = [], []
     while p2:
         q = add_nn(p2.pop(), p1[::1], 0)
         s = add_nn(q + shift, s, 1)
